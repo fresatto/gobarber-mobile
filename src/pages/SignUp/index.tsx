6848b0ec272c6432baabc1417b1ecc7fbbcde8a8
Gabriel Fresatto <gabrielfresatto@gmail.com>
2020-06-07 17:53:04 -0300

    Sign up page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,53 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-const SignIn: React.FC = () => {
-  return <Text>Sign Up</Text>;
+import Background from "../../components/Background";
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from "./styles";
+
+const SignUp: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  return (
+    <Background>
+      <Container>
+        <Image source={require("../../assets/logo.png")} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton>Cadastrar</SubmitButton>
+          <SignLink onPress={() => navigate("SignIn")}>
+            <SignLinkText>Voltar</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
+    </Background>
+  );
 };
 
-export default SignIn;
+export default SignUp;
